9b539308bb455da5a5499f982906e50863b848f8
Macetko-Dev <macetko.dev@gmail.com>
2025-04-08 22:15:23 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+    if(html.classList.contains('light')) {
+  // se tiver light mode, adicionar imagem ligth
+      img.setAttribute('src', './assets/avatar.png')
+    } else {
+  // se tiver sem ligth mode, manter a imagem normal   
+      img.setAttribute('src', './assets/avatar-ligth.png')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
